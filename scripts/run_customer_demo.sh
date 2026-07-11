3824fd14dc5583169d36ec56317227f438a9991a
AndrewGordienko <andrew.gordienko05@gmail.com>
2026-07-12 00:07:49 +0100

    make customer demo startup self contained

--- scripts/run_customer_demo.sh
+++ scripts/run_customer_demo.sh
@@ -2,4 +2,5 @@
 set -eu
 ROOT="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
 cd "$ROOT"
+export PYTHONPATH="$ROOT/src${PYTHONPATH:+:$PYTHONPATH}"
 exec "${PYTHON:-python3}" -m osc.web --port "${PORT:-8787}"
